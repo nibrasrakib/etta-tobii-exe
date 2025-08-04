7aca89782b52c53bd18fecd3fcc86de3128c1073
aravind-3105 <aravind.narayanan@students.iiit.ac.in>
2025-08-04 19:35:10 -0400

    Initial commit with working application and detailed readme for local application to stream tobii gaze data.

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,12 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from app.config import Config
-from flask_session import Session
-from flask_login import LoginManager
-import visual_library_plos as vl
 import logging
-import re
 
+from flask import Flask
+from flask_cors import CORS
 # TOBII 
 from flask_socketio import SocketIO
-from flask_cors import CORS
+
+from app.config import Config
+from flask_session import Session
 
 # setup logging
 logging.basicConfig(level=logging.DEBUG)
@@ -20,31 +16,14 @@ app.config.from_object(Config)
 
 # TOBII
 CORS(app)  # Enable CORS
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*",async_mode='threading')
 
-# database
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-# authentication
-login_manager = LoginManager(app)
 
 # enable sessions
 sess = Session()
 sess.init_app(app)
 
-# regular expressions
-tokenize = re.compile("[^\w\-]+")  # a token is composed of
-# alphanumerics or hyphens
-exclude = re.compile("(\d+|.)$")  # number or one-character tokens
-
-# Read stopword list
-stopwords = vl.read_stopwords(app.config['DATA_DIR'])
-
-# variables
-MINDF = 1
-
-from app import routes, routes_auth, routes_upload, utils, models
+from app import routes
 
 '''
 main
@@ -52,4 +31,3 @@ main
 if __name__ == '__main__':
 
     app.run('127.0.0.1', 5000, debug=True)
-    # app.run(processes=10, port=5000, debug=True)
